8fd54c7a0d212f36e220677adf0f891c26dd9fff
9Boxa4 <bogdanjovanovic10@gmail.com>
2022-02-03 14:04:26 +0100

    TASK12_ORGANIZACIJA_KODA

--- Projekat/app.js
+++ Projekat/app.js
@@ -1,14 +1,16 @@
 import Chatroom from "./chat.js";
 import ChatUI from "./ui.js";
 
+//DOM
+let formSend = document.querySelector(`#sendForm`);
+let inputSend = document.querySelector(`#send`);
+let lista = document.querySelector(`#ispisPor`);
 
+//Instance klasa
+let obj1 = new Chatroom(`general`, `Janko`);
+let is = new ChatUI(lista);
 
-
-let obj1 = new Chatroom(`general`, `Janko`)
-let obj2 = new Chatroom(`homework`, `Zarko`)
-let obj3 = new Chatroom(`JS`, `Stefan`)
-
-// console.log(obj1.username, obj1.room);
+//
 
 // obj1.addChat(`bla bla`)
 //     .then(() => {
@@ -18,39 +20,18 @@ let obj3 = new Chatroom(`JS`, `Stefan`)
 //         console.log(`Doslo je do greske ${err}`);
 //     })
 
-// obj2.getChats(()=>
-// {
-
-// })
-
+// ispis dokumenata iz db u konzoli
 // obj1.getChats(d => {
 //     console.log(d);
 // });
 
-let lista = document.querySelector(`#ispisPor`)
-let is = new ChatUI(lista)
-
-// console.log(lista);
-// console.log(is.lista);
-
-// obj1.templateUI(us,por,ts)
-// {
-//    let d =`${us.username}: ${por.message} <br> ${ts.createdAt}`;
-// }
-
+//uzimamo datu iz chat kolekcije
 obj1.getChats(d=>
     {
         is.templateLi(d);
     })
 
-//     obj1.getChats(d=>
-//         {
-//             formating(d)
-//         })
-
-let formSend = document.querySelector(`#sendForm`);
-let inputSend = document.querySelector(`#send`);
-
+//za dodavanje poruke
 formSend.addEventListener(`submit`, (event) =>
 {
     event.preventDefault();
@@ -64,23 +45,12 @@ formSend.addEventListener(`submit`, (event) =>
     .catch(err => {
         console.log(`Doslo je do greske ${err}`);
     })
-    //     obj1.addChat(poruka)
-//    .then(()=>
-//    {
-//        console.log(`Uspesno dodata poruka`);
-//    })
-//    .catch(err=>
-//     {
-//         console.log(`Doslo je do greske ${err}`);
-//     })
-
 
 });
 
-
+//updateovanje imena
         let formaUpdate = document.querySelector(`#updateForm`)
         let inputUpdate = document.querySelector(`#update`)
-        console.log(inputUpdate);
         formaUpdate.addEventListener(`submit`, event=>
         {
             event.preventDefault();
